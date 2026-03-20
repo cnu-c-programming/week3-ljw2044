6cb276ae774266587f6853a5e3f565d508cea5cb
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-03-20 10:15:02 +0900

    3-5c

---
+++ code/3-5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void my_sum(char type, int count, ...) {
+    va_list args;
+    va_start(args, count);
+
+    if (type == 'S') {
+        char *str1 = va_arg(args, char *);
+        char *str2 = va_arg(args, char *);
+        printf("%s %s\n", str1, str2);
+    } else if (type == 'C') {
+        for (int i = 0; i < count; i++) {
+            char c = (char)va_arg(args, int);
+            printf("%c", c);
+        }
+        printf("\n");
+    } else if (type == 'D') {
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            sum += va_arg(args, int);
+        }
+        printf("%d\n", sum);
+    }
+
+    va_end(args);
+}
+
+int main() {
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U');
+    my_sum('D', 4, 10, 20, 30, 40);
+    return 0;
+}
