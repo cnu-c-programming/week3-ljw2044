7b916f7fe049fd49349a8d791e97a91cedd50b30
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-03-20 10:14:42 +0900

    3-4c

---
+++ code/3-4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int max_of(int count, ...) {
+    va_list args;
+    va_start(args, count);
+
+    int max = va_arg(args, int);
+    for (int i = 1; i < count; i++) {
+        int num = va_arg(args, int);
+        if (num > max) {
+            max = num;
+        }
+    }
+
+    va_end(args);
+    return max;
+}
+
+int main() {
+    printf("%d\n", max_of(4, 5, 12, 3, 9));
+    printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+    printf("%d\n", max_of(3, 100, 200, 300));
+    return 0;
+}
