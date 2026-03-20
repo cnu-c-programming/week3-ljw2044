8b8a168879c308a80132d9e47d4ebeeaffa256de
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-03-20 10:10:06 +0900

    1-1c

---
+++ code/1-1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int sub(int x, int y) {
+    return x - y;
+}  
+
+int mul(int x, int y) {
+    return x * y;
+}  
+
+int div(int x, int y) {
+    if (y != 0) {
+        return x / y;
+    } else {
+        printf("Error: Division by zero\n");
+        return 0;
+    }
+}
+
+int main()
+{
+    int a = 10, b = 5;
+    printf("%d + %d = %d\n", a, b, add(a, b));
+    printf("%d - %d = %d\n", a, b, sub(a, b));
+    printf("%d * %d = %d\n", a, b, mul(a, b));
+    printf("%d / %d = %d\n", a, b, div(a, b));
+
+    return 0;
+}
